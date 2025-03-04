a2185ec9b088d5ac69f86ecc9f5ebb43ee6373f0
PannagaJA2 <pannagajavisionx@gmail.com>
2025-03-05 00:56:06 +0530

    add framer-motion dependency and implement HomePage component with layout and features (#4)

--- Landing-Page/src/App.jsx
+++ Landing-Page/src/App.jsx
@@ -1,13 +1,7 @@
-import './App.css'
+import HomePage from "./assets/pages/HomePage";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center text-blue-500">
-    Hello world!
-  </h1>
-    </>
-  )
+  return <HomePage />;
 }
 
-export default App
+export default App;
